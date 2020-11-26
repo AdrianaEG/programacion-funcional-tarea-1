7d765d48fc1006462cd16775f425ccd8b8b8c553
AdrianaEG <adriana.ngc438@gmail.com>
2020-11-25 21:22:31 -0300

    Agrego clase prog funcional

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,8 +1,19 @@
-export function multiplicarPor10(array) {}
+export function multiplicarPor10(array) {
+    const newArray =  array.map(element => element*10);
+    return newArray;
+}
 
-export function soloPares(array) {}
+export function soloPares(array) {
+    const evenNumbers = array.filter(element=>element%2==0);
+    return evenNumbers;
+}
 
-export function moverALaDerecha(array) {}
+export function moverALaDerecha(array) {
+    let ultimo = array[array.length - 1];
+    let newArray = [ultimo]; 
+    newArray = array.map(valor => newArray.push(valor));
+    return newArray; 
+}
 
 export function soloUnaPalabra(array) {}
 
@@ -12,8 +23,12 @@ export function soloVocales(array) {}
 
 export function todasVocalesIguales(array) {}
 
-export function duplicarMatriz(array) {}
+export function duplicarMatriz(array) {
+    let nuevoInput = array.map(unImput=>unImput.map(valor=>valor*2))
+    return nuevoInput;
+}
 
 export function mayoresDeEdadValidados(array) {}
 
-export function transformarObjetoEnArray(objeto) {}
+export function transformarObjetoEnArray(objeto) {
+}
